2ba5f058eee7e992376ba14b643f24715b837d6a
Perminus-Karuri <perminusdeng73@gmail.com>
2024-02-28 18:30:23 +0300

    python commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input ('Enter your name')
+age = input('Enter your age')
+location = input('Enter your location')
+print('My name is ' + name + ' I am ' + age + ' years old and I live in ' + location)
